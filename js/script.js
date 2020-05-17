9967d7f846da5025331f478dca7000dca87bdd28
huuluantvka2 <57451218+huuluantvka2@users.noreply.github.com>
2020-05-17 17:42:35 +0700

    the first time

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:3,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[2000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    })
+});
